f6a660788d5981d1356122d1623622a261470157
024x <satyendrabongi@gmail.com>
2023-01-09 12:14:41 +0000

    remove 10

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ async def echo_server(websocket, path):
   while True:
     message = await websocket.recv()
     await websocket.send(message)
-    await asyncio.sleep(10)
+    
     await websocket.send(message)
 start_server = websockets.serve(echo_server, '0.0.0.0', 5000)
 asyncio.get_event_loop().run_until_complete(start_server)
