6d371a71e6052f9c7c40c43123ac8d3f9ef74c11
024x <satyendrabongi@gmail.com>
2023-01-09 08:13:45 +0000

    k

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+@socketio.on('connect')
+def on_connect():
+  print('Client connected')
+
+@socketio.on('disconnect')
+def on_disconnect():
+  print('Client disconnected')
+
+@socketio.on('message')
+def on_message(message):
+  print('Message received: ' + message)
+  # Send a response back to the client
+  socketio.emit('response', {'data': 'This is a response'})
+
+if __name__ == '__main__':
+  socketio.run(app)
