4f8c0161d85d0c1719b532a6d8d75f1d527dabb2
024x <satyendrabongi@gmail.com>
2023-01-09 12:29:23 +0000

    oh

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import asyncio
 from websockets import serve
-
+import os
 async def echo(websocket):
     async for message in websocket:
         await websocket.send(message)
 
 async def main():
-    async with serve(echo, "0.0.0.0", 5000):
+    async with serve(echo, "0.0.0.0", os.getenv('PORT')):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
